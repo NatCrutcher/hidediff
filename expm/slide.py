06dcb7f515f7eb29f812ae0a9b96c0c8c4ff9a7c
Nathaniel Crutcher <nat3@ncrutcher.com>
2026-03-07 08:06:52 -0700

    Adjust match scoring by string offset.

--- expm/slide.py
+++ expm/slide.py
@@ -2,21 +2,25 @@
 
 import sys
 import argparse
+import math
 
 def comp_pos(a: str, b: str, a_offset: int) -> tuple[list[int], list[str]]:
     a_len = len(a)
     b_len = len(b)
     max_len = max(a_len, b_len)
+    ab_center_shift = (a_len - b_len) / 2
     comps = []    # Comparison values
     a_shift = []
     for ib in range(b_len):
         ia = ib + a_offset
         if ia < 0 or ia >= a_len:
             a_shift.append("_")
-            comps.append(0)
+            comps.append(0.0)
         else:
             a_shift.append(a[ia])
-            comp = 1 if a[ia] == b[ib] else 0
+            offset = 2 * math.log2(max_len - abs(ia - ib - ab_center_shift))
+            #offset = max_len - abs(ia - ib - ab_center_shift)
+            comp = offset if a[ia] == b[ib] else 0.0
             comps.append(comp)
     return comps, a_shift
 
@@ -30,7 +34,7 @@ def slide(a: str, b: str):
     for ip in range(positions):
         a_offset = a_len - ip - 1   # Start with one char overlap
         comps, asub = comp_pos(a, b, a_offset)
-        comp_str = ''.join(str(d) for d in comps)
+        comp_str = ' '.join(f"{v:2.0f}" for v in comps)
         print(comp_str, ' ', ''.join(asub))
 
 
@@ -42,8 +46,8 @@ def main() -> int:
     )
     parser.add_argument("strs", nargs=2, help="Two strings to compare")
     args = parser.parse_args()
-    print("1: ", args.strs[0])
-    print("2: ", args.strs[1])
+    #print("1: ", args.strs[0])
+    #print("2: ", args.strs[1])
     slide(args.strs[0], args.strs[1])
     return 0
 
